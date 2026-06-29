f4209c44f388354a1ca49852426972a8a582e12a
FJFC003 <e1729156727@uisrael.edu.ec>
2026-06-29 18:01:40 -0500

    Create DesviosOrdenOT.java

---
+++ gestalab/src/main/java/com/prototipo/gestalab/dominio/entidades/DesviosOrdenOT.java
@@ -0,0 +1,30 @@
+package com.prototipo.gestalab.dominio.entidades;
+
+
+public class DesviosOrdenOT {
+	
+	private int idDesviosOrdenOT;
+	private String descripcionDesviosOrdenOT;
+	private int puntosModificadosDesviosOrdenOT;
+	public int getIdDesviosOrdenOT() {
+		return idDesviosOrdenOT;
+	}
+	public void setIdDesviosOrdenOT(int idDesviosOrdenOT) {
+		this.idDesviosOrdenOT = idDesviosOrdenOT;
+	}
+	public String getDescripcionDesviosOrdenOT() {
+		return descripcionDesviosOrdenOT;
+	}
+	public void setDescripcionDesviosOrdenOT(String descripcionDesviosOrdenOT) {
+		this.descripcionDesviosOrdenOT = descripcionDesviosOrdenOT;
+	}
+	public int getPuntosModificadosDesviosOrdenOT() {
+		return puntosModificadosDesviosOrdenOT;
+	}
+	public void setPuntosModificadosDesviosOrdenOT(int puntosModificadosDesviosOrdenOT) {
+		this.puntosModificadosDesviosOrdenOT = puntosModificadosDesviosOrdenOT;
+	}
+	
+	
+
+}
