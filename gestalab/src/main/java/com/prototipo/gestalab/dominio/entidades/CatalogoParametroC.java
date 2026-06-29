40e28fa12cfaef9869c417b414a1befb7227a1ee
FJFC003 <e1729156727@uisrael.edu.ec>
2026-06-29 18:01:29 -0500

    Create CatalogoParametroC.java

---
+++ gestalab/src/main/java/com/prototipo/gestalab/dominio/entidades/CatalogoParametroC.java
@@ -0,0 +1,65 @@
+package com.prototipo.gestalab.dominio.entidades;
+
+
+public class CatalogoParametroC {
+	
+	private int idParametroC;
+	private String condicionParametroC;
+	private String ensayoParametroC;
+	private String tecnicaParametroC;
+	private String procedimientoInternoParametroC;
+	private String normaReferencialParametroC;
+	private String unidadParametroC;
+	private String rangoTrabajoParametroC;
+	public int getIdParametroC() {
+		return idParametroC;
+	}
+	public void setIdParametroC(int idParametroC) {
+		this.idParametroC = idParametroC;
+	}
+	public String getCondicionParametroC() {
+		return condicionParametroC;
+	}
+	public void setCondicionParametroC(String condicionParametroC) {
+		this.condicionParametroC = condicionParametroC;
+	}
+	public String getEnsayoParametroC() {
+		return ensayoParametroC;
+	}
+	public void setEnsayoParametroC(String ensayoParametroC) {
+		this.ensayoParametroC = ensayoParametroC;
+	}
+	public String getTecnicaParametroC() {
+		return tecnicaParametroC;
+	}
+	public void setTecnicaParametroC(String tecnicaParametroC) {
+		this.tecnicaParametroC = tecnicaParametroC;
+	}
+	public String getProcedimientoInternoParametroC() {
+		return procedimientoInternoParametroC;
+	}
+	public void setProcedimientoInternoParametroC(String procedimientoInternoParametroC) {
+		this.procedimientoInternoParametroC = procedimientoInternoParametroC;
+	}
+	public String getNormaReferencialParametroC() {
+		return normaReferencialParametroC;
+	}
+	public void setNormaReferencialParametroC(String normaReferencialParametroC) {
+		this.normaReferencialParametroC = normaReferencialParametroC;
+	}
+	public String getUnidadParametroC() {
+		return unidadParametroC;
+	}
+	public void setUnidadParametroC(String unidadParametroC) {
+		this.unidadParametroC = unidadParametroC;
+	}
+	public String getRangoTrabajoParametroC() {
+		return rangoTrabajoParametroC;
+	}
+	public void setRangoTrabajoParametroC(String rangoTrabajoParametroC) {
+		this.rangoTrabajoParametroC = rangoTrabajoParametroC;
+	}
+	
+	
+
+}
