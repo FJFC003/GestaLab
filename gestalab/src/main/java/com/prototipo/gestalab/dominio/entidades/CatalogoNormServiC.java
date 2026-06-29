2fb4c0b859cc80d981dc14e0818f4dde2607b52a
FJFC003 <e1729156727@uisrael.edu.ec>
2026-06-29 18:01:25 -0500

    Create CatalogoNormServiC.java

---
+++ gestalab/src/main/java/com/prototipo/gestalab/dominio/entidades/CatalogoNormServiC.java
@@ -0,0 +1,30 @@
+package com.prototipo.gestalab.dominio.entidades;
+
+
+public class CatalogoNormServiC {
+	
+	private int idCatalogoNormServiEntity;
+	private String parametroCatalogoNormServiEntity;
+	private String lmpCatalogoNormServiEntity;
+	public int getIdCatalogoNormServiEntity() {
+		return idCatalogoNormServiEntity;
+	}
+	public void setIdCatalogoNormServiEntity(int idCatalogoNormServiEntity) {
+		this.idCatalogoNormServiEntity = idCatalogoNormServiEntity;
+	}
+	public String getParametroCatalogoNormServiEntity() {
+		return parametroCatalogoNormServiEntity;
+	}
+	public void setParametroCatalogoNormServiEntity(String parametroCatalogoNormServiEntity) {
+		this.parametroCatalogoNormServiEntity = parametroCatalogoNormServiEntity;
+	}
+	public String getLmpCatalogoNormServiEntity() {
+		return lmpCatalogoNormServiEntity;
+	}
+	public void setLmpCatalogoNormServiEntity(String lmpCatalogoNormServiEntity) {
+		this.lmpCatalogoNormServiEntity = lmpCatalogoNormServiEntity;
+	}
+	
+	
+
+}
