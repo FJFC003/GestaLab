41d3eedf9fb76261a4cb7e1bc5f2a6ad7d59803d
FJFC003 <e1729156727@uisrael.edu.ec>
2026-06-29 18:01:54 -0500

    Create CatalogoNormServiCEntity.java

---
+++ gestalab/src/main/java/com/prototipo/gestalab/infraestructura/persistencia/jpa/CatalogoNormServiCEntity.java
@@ -0,0 +1,23 @@
+package com.prototipo.gestalab.infraestructura.persistencia.jpa;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Normas")
+public class CatalogoNormServiCEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idCatalogoNormServiEntity;
+	@Column(length = 80)
+	private String parametroCatalogoNormServiEntity;
+	private String lmpCatalogoNormServiEntity;
+
+}
